7abbe2931462676e3420ea2b87f3b28bc10f52c6
ZhangXiang <xiang4603@qq.com>
2018-12-25 22:32:30 +0800

    add second category

--- case/letao/public/a2/js/category-second.js
+++ case/letao/public/a2/js/category-second.js
@@ -1,27 +1,65 @@
-$(function(){
+$(function () {
     let page = 1;
     let pageSize = 10;
+    let fileUploadUrl = null;
+    $.ajax({
+        type: "get",
+        url: "/category/queryTopCategoryPaging",
+        data: {
+            page: page,
+            pageSize: pageSize
+        },
+        success: function (res) {
+            $.ajax({
+                type: "get",
+                url: "/category/queryTopCategoryPaging",
+                data: {
+                    page: page,
+                    pageSize: res.total
+                },
+                success: function (response) {
+                    let html = template("categoryFirstTpl", response);
+                    $("#categoryFirstBox").html(html);
+                }
+            })
+        }
+    })
+    $.ajax({
+        type: "get",
+        url: "/category/querySecondCategoryPaging",
+        data: {
+            page: page,
+            pageSize: pageSize
+        },
+        success: function(res){
+            let html = template("categorySecondTpl", res);
+            $("#categorySecondBox").html(html);
+        }
+    })
+    $("#fileUpload").fileupload({
+        dataType: 'json',
+        done: function (e, data) {
+            console.log(data);
+            fileUploadUrl = data.result.picAddr;
+            // console.log(data.result.picAddr);
+            // $("#showBrand").attr("src",fileUploadUrl);
+        }
+    })
+    $("#save").on("click", function () {
+        let brandName = $("[name='brandName']").val();
+        let categoryId = $("#categoryFirstBox").val();
         $.ajax({
-            type: "get",
-            url: "/category/queryTopCategoryPaging",
+            type: "post",
+            url: "/category/addSecondCategory",
             data: {
-                page: page,
-                pageSize: pageSize
+                brandName: brandName,
+                categoryId: categoryId,
+                brandLogo: fileUploadUrl,
+                hot: 1
             },
             success: function(res){
-                $.ajax({
-                    type: "get",
-                    url: "/category/queryTopCategoryPaging",
-                    data: {
-                        page: page,
-                        pageSize: res.total
-                    },
-                    success: function(response){
-                        let html = template("categoryFirstTpl", response);
-                        $("#categoryFirstBox").html(html);
-                    }
-                })
+                location.reload();
             }
         })
-    
+    })
 })
