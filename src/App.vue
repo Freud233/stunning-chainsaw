068dd1fa03f7855df9a85f672d26a665afca6e02
ZhangXiang <xiang4603@qq.com>
2023-05-15 14:27:17 +0800

    使用vite初始化项目

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,30 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
 <template>
-  <div class="text-2xl font-bold underline">
-    <Hello></Hello>
-    <n-space>
-      <n-button>Default</n-button>
-      <n-button type="tertiary">
-        Tertiary
-      </n-button>
-      <n-button type="primary">
-        Primary
-      </n-button>
-      <n-button type="info">
-        Info
-      </n-button>
-      <n-button type="success">
-        Success
-      </n-button>
-      <n-button type="warning">
-        Warning
-      </n-button>
-      <n-button type="error">
-        Error
-      </n-button>
-    </n-space>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
   </div>
+  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<script>
-
-export default {
-  name: "App",
-};
-</script>
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
--- src/components/Hello.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div>
-        我是hello
-    </div>
-</template>
-
-
-<style lang="scss" scoped></style>
