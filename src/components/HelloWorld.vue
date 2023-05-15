e92e56d222bbeb78e2b277f96c0a9741f8b5b920
ZhangXiang <xiang4603@qq.com>
2023-05-15 14:52:13 +0800

    配置自动导入vue组件和naiveui组件

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -14,23 +14,41 @@ const count = ref(0)
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
-      测试哈哈
       <code>components/HelloWorld.vue</code> to test HMR
     </p>
   </div>
 
   <p>
     Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>, 测试自动导入组件 (automatic component
+    import)
   </p>
   <p>
     Install
     <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
     in your IDE for a better DX
   </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <n-space>
+    <n-button>Default</n-button>
+    <n-button type="tertiary">
+      Tertiary
+    </n-button>
+    <n-button type="primary">
+      Primary
+    </n-button>
+    <n-button type="info">
+      Info
+    </n-button>
+    <n-button type="success">
+      Success
+    </n-button>
+    <n-button type="warning">
+      Warning
+    </n-button>
+    <n-button type="error">
+      Error
+    </n-button>
+  </n-space>
 </template>
 
 <style scoped>
