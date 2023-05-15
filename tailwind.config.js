0348cb807fc6ce912f472f7192ea4da7cbeafe3e
ZhangXiang <xiang4603@qq.com>
2023-05-15 21:11:32 +0800

    feat(flowbite): 配置flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{vue,js,ts}'],
+  content: [
+    './src/**/*.{vue,js,ts}',
+    './node_modules/flowbite/**/*.js',
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
 };
