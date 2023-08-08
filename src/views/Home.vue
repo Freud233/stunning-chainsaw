65575c7a58952bbe2a3580ea837c42e0bf8c42ad
ZhangXiang <xiang4603@qq.com>
2023-08-08 22:18:30 +0800

    过滤**符号

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,9 +5,12 @@
         </div> -->
         <h1 class="w-4/5 m-auto mt-5 text-4xl">输入要格式化的文字</h1>
         <textarea type="text" class=" block w-4/5 m-auto mt-5" v-model="text" rows="5" @change="textReplace" />
-        <button @click="copyText"
-            class="block m-auto mt-4 bg-green-300 text-gray-800 shadow-xl  w-48 h-12 text-xl border-4 border-red-300">复制文字</button>
-        <div class="w-4/5 m-auto mt-5">{{ text }}</div>
+        <div class="h-40 flex justify-center items-center">
+            <button @click="copyText"
+                class="block m-auto my-4 hover:border hover:duration-75 hover:w-52 hover:h-16 hover:text-2xl bg-green-300 text-gray-800 shadow-xl  w-48 h-12 text-xl ">
+                复制文字</button>
+        </div>
+        <div class="w-4/5 m-auto my-5">{{ text }}</div>
     </div>
 </template>
   
@@ -16,10 +19,10 @@ import { useMessage } from 'naive-ui'
 const text = ref('')
 const message = useMessage()
 const textReplace = () => {
-    text.value = text.value.replace(/\s*/g, '')
+    text.value = text.value.replace(/\s*/g, '').replace(/\*\*/g, '')
         .replace(/([^1-9])?(\d+)\./g, (match, before, number) => {
             return (before ? before + '<br>' : '') + number + '、';
-        });
+        })
 }
 
 const copyText = () => {
